8d7ee9a241eb37380a07b4ec1a56547729d437f1
Patrick McFadden <pemcfadden@gmail.com>
2022-05-10 15:14:16 -0600

    [pmcfadden] Adding listeners on 80 and 443 for ALB

--- alb.tf
+++ alb.tf
@@ -64,8 +64,14 @@ module "alb_sg" {
 
   ingress_with_cidr_blocks = [
     {
-      from_port   = var.connect_api_port
-      to_port     = var.connect_api_port
+      from_port   = "443"
+      to_port     = "443"
+      protocol    = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = "80"
+      to_port     = "80"
       protocol    = "tcp"
       cidr_blocks = "0.0.0.0/0"
     }
