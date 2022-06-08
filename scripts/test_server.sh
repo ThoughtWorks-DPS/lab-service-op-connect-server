1f08a4d0d6d57fe2c62af4f6e6375f8a00ef9e0c
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-06-08 17:00:13 -0500

    [ncheneweth] fix monitor error, add smoke test
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

---
+++ scripts/test_server.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+export INSTANCE=$1
+export VAULT=$2
+
+if [[ "${INSTANCE}" == "test" ]]; then
+  export OP_CONNECT_HOST=https://test.op.twdps.io
+  export OP_CONNECT_TOKEN=${TEST_OP_CONNECT_TOKEN}
+fi
+
+cat <<EOF > integration_test.tpl
+{{ op://${VAULT}/integration-test/key }}
+EOF
+
+op inject -i integration_test.tpl -o integration_test.env
+cat integration_test.env | grep twdps
